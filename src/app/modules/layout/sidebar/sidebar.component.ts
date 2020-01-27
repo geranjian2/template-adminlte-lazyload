7e5f2b5cde2993e4dcdbd2f8fc2fddc5402b958b
Andres TAE <desarrolloweb2@tae-ltda.com>
2020-01-27 18:00:51 -0500

    ajuste menu

--- src/app/modules/layout/sidebar/sidebar.component.ts
+++ src/app/modules/layout/sidebar/sidebar.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { SidebarService } from 'src/app/service/service.index';
+
+
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
@@ -6,7 +9,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SidebarComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    public sidebarService: SidebarService,
+  ) { }
 
   ngOnInit() {
     console.log('sidebar');
